200eb560d5b7a2915da07a5fb674a953aed6c3b2
athagita <x2e9020@chiba-u.jp>
2015-01-10 17:44:30 +0900

    update
    家と背景がスクロールする

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ window.onload = function(){
 
 	var game = new Game(1136,640);
 	game.fps = 24;
-	game.preload('img/bg1.png','img/end.png','img/chara1.png');
+	game.preload('img/bg1.png','img/bg2.png','img/end.png','img/chara1.png','img/home2.png');
 
 	var sceneX = 1136;
 	var sceneY = 640;
@@ -29,6 +29,8 @@ window.onload = function(){
 
 		var createGameScene = function(){
 			var scene = new Scene();
+			var SCROLL_SPEED = 10;
+
 			var label = new Label('game');
 
 			scene.addChild(label);
@@ -39,12 +41,47 @@ window.onload = function(){
 
 			scene.addChild(bg1);
 
+			var bg2 = new Sprite(1136,640);
+			bg2.image = game.assets['img/bg2.png'];
+			bg2.moveTo(1136,0);
+
+			scene.addChild(bg2);
+
+			scene.addEventListener(Event.ENTER_FRAME, function(){
+				bg1.x -= SCROLL_SPEED;
+				bg2.x -= SCROLL_SPEED;
+				if(bg1.x <= -1136){
+					bg1.x = 1136;
+				}
+				if(bg2.x <= -1136){
+					bg2.x = 1136;
+				}
+
+
+			});
+
 			var santa = new Sprite(361,163);
 			santa.image = game.assets['img/chara1.png'];
 			santa.moveTo(100,50);
 			santa.scaleX = -1;
-
 			scene.addChild(santa);
+
+			var home = new Sprite(320,320);
+			home.image  = game.assets['img/home2.png'];
+			home.moveTo( scene.width  , scene.height - [home.height/1.5 + 20] );
+			home.scaleX = 0.5;
+			home.scaleY = 0.5;
+			scene.addChild(home);
+
+			scene.addEventListener(Event.ENTER_FRAME, function(){
+				home.x -= SCROLL_SPEED - 2;
+
+				if(home.x <= -[home.width]){
+				home.x = scene.width + 500;
+				}
+
+			});
+
 
 
 			scene.addEventListener(Event.TOUCH_START, function(e){
